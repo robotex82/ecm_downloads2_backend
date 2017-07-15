8c61930cec983780edb1c0acc01f367b0b2beea7
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-07-15 12:46:30 +0200

    Add route to toggle published status.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Ecm::Downloads::Backend::Engine.routes.draw do
   backend_resources :download_categories
   backend_resources :downloads do
     member { get :download }
+    member { post :toggle_published }
   end
 
   root to: 'home#index'
