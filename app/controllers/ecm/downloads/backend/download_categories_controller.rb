84837c87f893d174811b797a65e98667cc45eb2a
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-03-26 19:53:05 +0200

    #cleanup

--- app/controllers/ecm/downloads/backend/download_categories_controller.rb
+++ app/controllers/ecm/downloads/backend/download_categories_controller.rb
@@ -1,20 +1,26 @@
-class Ecm::Downloads::Backend::DownloadCategoriesController < Itsf::Backend::Resource::BaseController
-  include ResourcesController::Sorting
-  include ResourcesController::FriendlyIdConcern
+module Ecm
+  module Downloads
+    module Backend
+      class DownloadCategoriesController < Itsf::Backend::Resource::BaseController
+        include ResourcesController::Sorting
+        include ResourcesController::FriendlyIdConcern
 
-  def self.resource_class
-    Ecm::Downloads::DownloadCategory
-  end
+        def self.resource_class
+          Ecm::Downloads::DownloadCategory
+        end
 
-  private
+        private
 
-  def permitted_params
-    params
-      .require(:download_category)
-      .permit(:name, :description, :locale)
-  end
+        def permitted_params
+          params
+            .require(:download_category)
+            .permit(:name, :description, :locale)
+        end
 
-  def load_resource
-    load_scope.friendly.find(params[:id])
+        def load_resource
+          load_scope.friendly.find(params[:id])
+        end
+      end
+    end
   end
 end
--- app/controllers/ecm/downloads/backend/downloads_controller.rb
+++ app/controllers/ecm/downloads/backend/downloads_controller.rb
@@ -1,25 +1,31 @@
-class Ecm::Downloads::Backend::DownloadsController < Itsf::Backend::Resource::BaseController
-  include ResourcesController::Sorting
-  include ResourcesController::FriendlyIdConcern
+module Ecm
+  module Downloads
+    module Backend
+      class DownloadsController < Itsf::Backend::Resource::BaseController
+        include ResourcesController::Sorting
+        include ResourcesController::FriendlyIdConcern
 
-  def self.resource_class
-    Ecm::Downloads::Download
-  end
+        def self.resource_class
+          Ecm::Downloads::Download
+        end
 
-  def download
-    @download = Ecm::Downloads::Download.friendly.find(params[:id]).decorate
-    redirect_to @download.asset.expiring_url(100)
-  end
+        def download
+          @download = Ecm::Downloads::Download.friendly.find(params[:id]).decorate
+          redirect_to @download.asset.expiring_url(100)
+        end
 
-  private
+        private
 
-  def permitted_params
-    params
-      .require(:download)
-      .permit(:download_category_id, :asset, :name, :description, :published)
-  end
+        def permitted_params
+          params
+            .require(:download)
+            .permit(:download_category_id, :asset, :name, :description, :published)
+        end
 
-  def load_resource
-    load_scope.friendly.find(params[:id])
+        def load_resource
+          load_scope.friendly.find(params[:id])
+        end
+      end
+    end
   end
 end
